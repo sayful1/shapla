ecb043af9969bc606f7048aacfff68389bf3c88c
Sayful Islam <sayful.islam001@gmail.com>
2017-08-04 23:02:01 +0600

    Added customizer option to modify blog layout and others

--- inc/customizer/blog.php
+++ inc/customizer/blog.php
@@ -9,6 +9,14 @@ $shapla->customizer->add_section( 'blog_section', array(
 	'title'       => __( 'Blog Post Meta', 'shapla' ),
 	'description' => __( 'Customise meta information for the categories, tags and comments.', 'shapla' ),
 	'panel'       => 'blog_panel',
+	'priority'    => 20,
+) );
+
+// Add new section
+$shapla->customizer->add_section( 'blog_layout_section', array(
+	'title'       => __( 'Blog Layouts & Styles', 'shapla' ),
+	'description' => __( 'Customise your site blog layouts and styles.', 'shapla' ),
+	'panel'       => 'blog_panel',
 	'priority'    => 10,
 ) );
 
@@ -60,4 +68,37 @@ $shapla->customizer->add_field( [
 	'label'    => __( 'Show comments link', 'shapla' ),
 	'default'  => true,
 	'priority' => 60,
+] );
+$shapla->customizer->add_field( [
+	'settings' => 'blog_layout',
+	'type'     => 'select',
+	'section'  => 'blog_layout_section',
+	'label'    => __( 'Blog layout', 'shapla' ),
+	'default'  => 'grid',
+	'priority' => 10,
+	'choices'  => [
+		'default' => esc_html__( 'Default', 'shapla' ),
+		'grid'    => esc_html__( 'Grid', 'shapla' ),
+	]
+] );
+$shapla->customizer->add_field( [
+	'settings'    => 'blog_date_format',
+	'type'        => 'select',
+	'section'     => 'blog_layout_section',
+	'label'       => __( 'Blog date format', 'shapla' ),
+	'description' => __( 'Default date format is format you set from Settings --> General --> Date Format', 'shapla' ),
+	'default'     => 'human',
+	'priority'    => 10,
+	'choices'     => [
+		'default' => esc_html__( 'Default', 'shapla' ),
+		'human'   => esc_html__( 'Human readable', 'shapla' ),
+	]
+] );
+$shapla->customizer->add_field( [
+	'settings' => 'blog_excerpt_length',
+	'type'     => 'number',
+	'section'  => 'blog_layout_section',
+	'label'    => __( 'Excerpt length', 'shapla' ),
+	'default'  => 20,
+	'priority' => 20,
 ] );
