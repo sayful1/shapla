027a460815ed26cc360e550e598c830ce6b217da
Sayful Islam <sayful.islam001@gmail.com>
2018-02-03 14:31:54 +0600

    Update Shapla_Customize_Control class core.

--- inc/customizer/controls/class-shapla-customize-control.php
+++ inc/customizer/controls/class-shapla-customize-control.php
@@ -5,12 +5,45 @@ if ( ! defined( 'ABSPATH' ) ) {
 }
 
 class Shapla_Customize_Control extends WP_Customize_Control {
+	/**
+	 * Used to automatically generate all CSS output.
+	 *
+	 * @access public
+	 * @var array
+	 */
+	public $output = array();
 
 	/**
-	 * Control's Type.
+	 * Data type
+	 *
+	 * @access public
 	 * @var string
 	 */
-	public $type = 'text';
+	public $option_type = 'theme_mod';
+
+	/**
+	 * Option name (if using options).
+	 *
+	 * @access public
+	 * @var string
+	 */
+	public $option_name = false;
+
+	/**
+	 * The shapla_config we're using for this control
+	 *
+	 * @access public
+	 * @var string
+	 */
+	public $shapla_config = 'global';
+
+	/**
+	 * Whitelisting the "required" argument.
+	 *
+	 * @access public
+	 * @var array
+	 */
+	public $required = array();
 
 	/**
 	 * Enqueue scripts and styles for the custom control.
@@ -42,6 +75,39 @@ class Shapla_Customize_Control extends WP_Customize_Control {
 	 */
 	public function to_json() {
 		parent::to_json();
+
+		// Default.
+		$this->json['default'] = $this->setting->default;
+		if ( isset( $this->default ) ) {
+			$this->json['default'] = $this->default;
+		}
+		// Required.
+		$this->json['required'] = $this->required;
+		// Output.
+		$this->json['output'] = $this->output;
+		// Value.
+		$this->json['value'] = $this->value();
+		// Choices.
+		$this->json['choices'] = $this->choices;
+		// The link.
+		$this->json['link'] = $this->get_link();
+		// The ID.
+		$this->json['id'] = $this->id;
+		// Translation strings.
+		$this->json['l10n'] = $this->l10n();
+		// The ajaxurl in case we need it.
+		$this->json['ajaxurl'] = admin_url( 'admin-ajax.php' );
+		// Input attributes.
+		$this->json['inputAttrs'] = '';
+		foreach ( $this->input_attrs as $attr => $value ) {
+			$this->json['inputAttrs'] .= $attr . '="' . esc_attr( $value ) . '" ';
+		}
+		// The shapla-config.
+		$this->json['shaplaConfig'] = $this->shapla_config;
+		// The option-type.
+		$this->json['shaplaOptionType'] = $this->option_type;
+		// The option-name.
+		$this->json['shaplaOptionName'] = $this->option_name;
 	}
 
 	/**
@@ -66,5 +132,16 @@ class Shapla_Customize_Control extends WP_Customize_Control {
 	 * @see WP_Customize_Control::print_template()
 	 */
 	protected function content_template() {
+	}
+
+	/**
+	 * Returns an array of translation strings.
+	 *
+	 * @access protected
+	 * @since 3.0.0
+	 * @return array
+	 */
+	protected function l10n() {
+		return array();
 	}
 }
