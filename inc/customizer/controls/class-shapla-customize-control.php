b04d1f7b8a9469c68e9d961ea5f5fd305ca39894
Sayful Islam <sayful.islam001@gmail.com>
2018-02-10 20:58:57 +0600

    Fixed javaScript is not loading properly.

--- inc/customizer/controls/class-shapla-customize-control.php
+++ inc/customizer/controls/class-shapla-customize-control.php
@@ -77,9 +77,9 @@ if ( ! class_exists( 'Shapla_Customize_Control' ) ) {
 			);
 
 			// Enqueue selectWoo.
-			wp_enqueue_script( 'selectWoo', $asset_url . 'assets/libs/selectWoo/js/selectWoo.full.js', array( 'jquery' ), '1.0.1', true );
-			wp_enqueue_style( 'selectWoo', $asset_url . 'assets/libs/selectWoo/css/selectWoo.css', array(), '1.0.1' );
-			wp_enqueue_style( 'kirki-selectWoo', $asset_url . 'assets/libs/selectWoo/kirki.css', null );
+			wp_enqueue_script( 'selectWoo', $asset_url . '/libs/selectWoo/js/selectWoo.full.js', array( 'jquery' ), '1.0.1', true );
+			wp_enqueue_style( 'selectWoo', $asset_url . '/libs/selectWoo/css/selectWoo.css', array(), '1.0.1' );
+			wp_enqueue_style( 'kirki-selectWoo', $asset_url . '/libs/selectWoo/kirki.css', null );
 
 			wp_enqueue_script( 'shapla-customize', $asset_url . '/js/customize' . $suffix . '.js',
 				array(
