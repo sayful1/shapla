d53865b11a4f415b4ab684c0ed40c94cfe3da73f
Sayful Islam <sayful.islam001@gmail.com>
2020-04-03 00:13:09 +0530

    Remove product search form javaScript

--- assets/src/public/ShaplaSearch.js
+++ assets/src/public/ShaplaSearch.js
@@ -1,7 +1,9 @@
 class ShaplaSearch {
+	/**
+	 * Class constructor
+	 */
 	constructor() {
 		ShaplaSearch.defaultSearch();
-		ShaplaSearch.productSearch();
 	}
 
 	static defaultSearch() {
@@ -20,46 +22,6 @@ class ShaplaSearch {
 			if (!menuSearch.contains(e.target)) {
 				menuSearch.classList.remove('shapla-main-menu-search-open');
 			}
-		});
-	}
-
-	static productSearch() {
-		let productSearch = document.querySelector('.shapla-product-search');
-		if (!productSearch) return;
-
-		let catList = productSearch.querySelector('.shapla-cat-list');
-		if (!catList) return;
-
-		let form = productSearch.querySelector('form'),
-			searchInput = productSearch.querySelector('input[type="search"]'),
-			searchLabel = productSearch.querySelector('.nav-search-label'),
-			defaultLabel = searchLabel.getAttribute('data-default'),
-			defaultVal = catList.value;
-
-		if (defaultVal === '') {
-			searchLabel.textContent = defaultLabel;
-		} else {
-			searchLabel.textContent = defaultVal;
-		}
-
-		catList.addEventListener('change', function () {
-			let label = catList.options[catList.selectedIndex].text;
-			let selectText = this.value;
-			if (selectText === '') {
-				searchLabel.textContent = defaultLabel;
-			} else {
-				searchLabel.textContent = label;
-			}
-
-			searchInput.focus();
-		});
-
-		searchInput.addEventListener('focus', () => {
-			form.classList.add('is-focused');
-		});
-
-		searchInput.addEventListener('blur', () => {
-			form.classList.remove('is-focused');
 		});
 	}
 }
