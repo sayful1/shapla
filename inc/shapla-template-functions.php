e1cbebd74f827286e00d24ec073bb6b2ec5893ed
Sayful Islam <sayful.islam001@gmail.com>
2022-08-02 22:43:03 +0600

    Update search-form design.

--- inc/shapla-template-functions.php
+++ inc/shapla-template-functions.php
@@ -314,20 +314,9 @@ if ( ! function_exists( 'shapla_search_modal' ) ) {
 										   class="input input--search search-form__input" autocomplete="off"
 										   placeholder="<?php echo esc_attr_x( 'Search &hellip;', 'placeholder', 'shapla' ) ?>"
 										   value="<?php the_search_query() ?>" name="s">
-									<button type="submit" class="search-input__button">
-										<span class="search-input__label">Submit</span>
-										<svg role="img" class="search-input__arrow-icon" width="30" height="28"
-											 viewBox="0 0 30 28" fill="inherit" xmlns="http://www.w3.org/2000/svg">
-											<g clip-path="url(#clip0)">
-												<path d="M16.1279 0L29.9121 13.7842L16.1279 27.5684L14.8095 26.25L26.3378 14.7217H-6.10352e-05V12.8467H26.3378L14.8095 1.31844L16.1279 0Z"
-													  fill="inherit"></path>
-											</g>
-											<defs>
-												<clipPath id="clip0">
-													<rect width="29.9121" height="27.5684" fill="white"></rect>
-												</clipPath>
-											</defs>
-										</svg>
+									<button type="submit" class="search-input__button alt">
+										<span class="search-input__label screen-reader-text">Submit</span>
+										<?php echo \Shapla\Helpers\SvgIcon::get_svg( 'ui', 'arrow_forward', 24 ) ?>
 									</button>
 								</form>
 							</div>
